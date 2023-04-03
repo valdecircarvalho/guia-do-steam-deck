88223558935cf7237df22f6f4d632e2d0b831b2a
valdecircarvalho <valdecir.carvalho@outlook.com>
2023-04-03 00:03:28 -0300

    Publicado em: 2023-04-03_00:03:27 | Computador: SURFACE-VC

---
+++ content/guia/news/chaveiros-steam-deck.md
@@ -0,0 +1,8 @@
+---
+title: 'Chaveiros Steam Deck'
+date: 2023-04-02 T00:00:00+00:00
+summary: ''
+slug: 'chaveiros-steam-deck'
+---
+
+Texto
--- push.ps1
+++ push.ps1
@@ -1,10 +1,19 @@
+# Capturar a data e hora atual
+$dataHora = Get-Date -Format "yyyy-MM-dd_HH:mm:ss"
+
+# Capturar o nome do computador
+$nomeComputador = $env:COMPUTERNAME
+
+# Definir a mensagem do commit incluindo a data e hora capturadas
+$mensagemCommit = "Publicado em: $dataHora | Computador: $nomeComputador"
+
 Write-Host "Publicando site..."
 git pull
 Write-Host "Update arquivos no repo"
-git add . && git commit -m "surface-files" && git commit && git push
+git add . && git commit -m "$mensagemCommit" && git push
 Write-host "Build do site..."
 hugo --cleanDestinationDir --gc --minify
 Write-host "Update arquivos estáticos no repo"
 git pull
-git add . && git commit -m "surface-files" && git commit && git push
+git add . && git commit -m "$mensagemCommit" && git push
 Write-Host "Finalizando publicação."
